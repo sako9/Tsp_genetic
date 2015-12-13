55a9d1ecbe8e10fa0762e98f9ebe73ce599ef33c
sako9 <sam.nwosu@yahoo.com>
2015-12-13 13:13:56 -0500

    initial commit

---
+++ Point.java
@@ -0,0 +1,18 @@
+package genetic;
+
+import java.util.ArrayList;
+
+public class Point {
+	public double x;
+	public double y;
+	
+	public Point(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString(){
+		return "(" + x +"," + y +")";
+	}
+
+}
